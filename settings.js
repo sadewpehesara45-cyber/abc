e089a1231ceb905b01cb90bade9fdbfdc1cd42fb
sadewpehesara45-cyber <sadewpehesara45@gmail.com>
2025-10-18 20:44:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=181209eeba636a48' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
+𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=141420249b611bdf
